112ae7b84b58da8e099a75b91cb50f74e68a382e
童天相 <Heikunan@users.noreply.github.com>
2017-08-02 22:49:14 +0800

    '123'

--- Public/src/js/moviecontain.js
+++ Public/src/js/moviecontain.js
@@ -1,4 +1,10 @@
 let myurl = window.location.href.split("?id=");
 axios.get('/getMovie/'+myurl[1]).then(function (ans) {
-    console.log(ans);
+    console.log(ans.data[0]);
+    $('.panel-heading-title').html(ans.data[0].name+`<small>(${ans.data[0].release})</small>`);
+    $('.movie-img').attr('src',ans.data[0].movieimg);
+    $('.gyf-directors').html(`导演：`+ans.data[0].directors);
+    $('.gyf-score').html(`评分：`+ans.data[0].score);
+    $('.gyf-comment').html(`所属类别：`+ans.data[0].comment);
+
 });
