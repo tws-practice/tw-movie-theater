bdb51f9a6e9fe48e369107f8bdd86b628f1370b3
童天相 <Heikunan@users.noreply.github.com>
2017-08-02 14:16:10 +0800

    '123'

--- server.js
+++ server.js
@@ -1,53 +1,26 @@
-var express = require('express');
-var orm = require('orm');
-var app = express();
+let express = require('express');
+let app = express();
 
-app.use(orm.express("sqlite:movie.db", {
-    /*评论的数据库*/
-    define: function (db, models, next) {
-        /*评论的models*/
-        models.T_comment = db.define("T_comment", {
-            userid   : String,
-            comment     : Number, // FLOAT
-            contentdate  : Boolean,
-            movieid     : String, // BLOB/BINARY
-        });
-        /*电影的models*/
-        models.T_movie = db.define("T_movie", {
-            userid   : String,
-            comment     : Number, // FLOAT
-            contentdate  : Boolean,
-            movieid     : String, // BLOB/BINARY
-        });
-        /*用户的models*/
-        models.T_users = db.define("T_users", {
-            name:String,
-            pasword:String,
-            content:String
-        });
-        /*类别的models*/
-        models.T_classes = db.define("T_classes", {
-            name:String
-        }, {
-        });
-        next();
-    }
-}));
+app.use(express.static('Public'));
 
-app.get("/", function (req, res) {
-    // req.models is a reference to models used above in define()
-    req.models.T_comment.find({movieid: "8888" }, function (err, movies) {
-        // SQL: "SELECT * FROM person WHERE surname = 'Doe'"
-        if (err) throw err;
+const myMovie = [{
+    name:'大鱼',
+    score:'8.7',
+    movieimg :'https://img3.doubanio.com/view/movie_poster_cover/spst/public/p692813374.jpg'
+},{
+    name:'黑鹰坠落',
+    score:'8.5',
+    movieimg :'https://img3.doubanio.com/view/movie_poster_cover/spst/public/p1910900710.jpg'
+},{
+    name:'怪兽电力公司',
+    score:'8.6',
+    movieimg :'https://img1.doubanio.com/view/movie_poster_cover/spst/public/p1805127697.jpg'
+}];
 
-        console.log("People found: %d %s", movies.length,movies[0].movieid);
-        console.log( movies[0]);
-
-        movies[0].userid = "000001";
-        movies[0].save(function (err) {
-            // err.msg = "under-age";
-        });
-    });
+app.get('/all',function (req,res) {
+    res.json(myMovie);
 });
 
-app.listen(8081);
+
+
+app.listen('3002',()=>(console.log('123')));
