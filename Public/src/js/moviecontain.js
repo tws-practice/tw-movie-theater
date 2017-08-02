9c49c1396d02a3d8f800146a6a89352acd2422cb
童天相 <Heikunan@users.noreply.github.com>
2017-08-02 23:08:23 +0800

    '123'

--- Public/src/js/moviecontain.js
+++ Public/src/js/moviecontain.js
@@ -6,5 +6,6 @@ axios.get('/getMovie/'+myurl[1]).then(function (ans) {
     $('.gyf-directors').html(`导演：`+ans.data[0].directors);
     $('.gyf-score').html(`评分：`+ans.data[0].score);
     $('.gyf-comment').html(`所属类别：`+ans.data[0].comment);
-
+    $('.gyf-casts').html(`主演：`+ans.data[0].casts);
+    $('.gyf-detail').html(`  `+ans.data[0].detail);
 });
