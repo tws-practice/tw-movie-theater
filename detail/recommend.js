24ffa31f41386f6df5fba8ba2a5e28a8af240ad4
ashui <ashui@ashuihouse.cn>
2017-08-03 10:44:22 +0800

    recommend

--- detail/recommend.js
+++ detail/recommend.js
@@ -16,20 +16,32 @@ function addRecommendSql(id) {
 }
 function addRecommends(data){
     data.forEach(function(item, index, array){
+        let number=0;
         if(index<12){
-            addRecommendone(item);
+            if(number<6){
+                addRecommendOne(item,'rom1');
+            }
+            else{
+                addRecommendOne(item,'rom2');
+            }
+            number++;
         }
     })
 }
-function addRecommendone(oneMovie) {
+function addRecommendOne(oneMovie,rom='rom1') {
     let $divBox=$(`<div class="col-xs-6 col-sm-4  col-md-2 "></div>`);
     let $dlBox=$(`<dl></dl>`);
     let $dtBox=$(`<dt></dt>`);
-    let $aBox=$(`<a href="https://localhost:9998/movies/${oneMovie.id}"></a>`);
-    let $imgBox=$(`<img src="${oneMovie.img}" alt="${oneMovie.name}" class="">`);
+    let $aBox=$(`<a href="${oneMovie.alt}"></a>`);
+    let $imgBox=$(`<img src="${oneMovie.image}" alt="${oneMovie.title}" class="">`);
     let $ddBox=$(`<dd></dd>`);
-    let $aTiltleBox=$(`<a href="https://localhost:9998/movies/${oneMovie.id}" class="">"${oneMovie.name}"</a>`);
-    $("#row1").append($divBox);
+    let $aTiltleBox=$(`<a href="${oneMovie.alt}" class="">"${oneMovie.title}"</a>`);
+    if(rom=='rom1'){
+        $(`#row1`).append($divBox);
+    }
+    if(rom=='rom2'){
+        $(`#row2`).append($divBox);
+    }
     $divBox.append($dlBox);
     $dlBox.append($dtBox);
     $dtBox.append($aBox);
@@ -37,3 +49,203 @@ function addRecommendone(oneMovie) {
     $dlBox.append($ddBox);
     $ddBox.append($aTiltleBox);
 }
+
+const movies=[
+    {
+    "id": 837,
+    "alt": "https://movie.douban.com/subject/1316510/",
+    "year": 1993,
+    "title": "射雕英雄传之东成西就",
+    "rating": "8.7",
+    "original_title": "射鵰英雄傳之東成西就",
+    "directors": "刘镇伟",
+    "casts": "梁朝伟,林青霞,张国荣",
+    "image": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p1993903133.jpg",
+    "name": "运动",
+    "movie_id": 1316510,
+    "genre_id": 27
+},
+    {
+        "id": 1044,
+        "alt": "https://movie.douban.com/subject/1306249/",
+        "year": 1993,
+        "title": "唐伯虎点秋香",
+        "rating": "8.4",
+        "original_title": "唐伯虎點秋香",
+        "directors": "李力持",
+        "casts": "周星驰,巩俐,陈百祥",
+        "image": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p1946455272.jpg",
+        "name": "运动",
+        "movie_id": 1306249,
+        "genre_id": 27
+    },
+    {
+        "id": 1126,
+        "alt": "https://movie.douban.com/subject/1303394/",
+        "year": 1993,
+        "title": "青蛇",
+        "rating": "8.4",
+        "original_title": "青蛇",
+        "directors": "徐克",
+        "casts": "张曼玉,王祖贤,赵文卓",
+        "image": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p584021784.jpg",
+        "name": "运动",
+        "movie_id": 1303394,
+        "genre_id": 27
+    },
+    {
+        "id": 1126,
+        "alt": "https://movie.douban.com/subject/1303394/",
+        "year": 1993,
+        "title": "青蛇",
+        "rating": "8.4",
+        "original_title": "青蛇",
+        "directors": "徐克",
+        "casts": "张曼玉,王祖贤,赵文卓",
+        "image": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p584021784.jpg",
+        "name": "运动",
+        "movie_id": 1303394,
+        "genre_id": 27
+    },
+    {
+        "id": 1126,
+        "alt": "https://movie.douban.com/subject/1303394/",
+        "year": 1993,
+        "title": "青蛇",
+        "rating": "8.4",
+        "original_title": "青蛇",
+        "directors": "徐克",
+        "casts": "张曼玉,王祖贤,赵文卓",
+        "image": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p584021784.jpg",
+        "name": "运动",
+        "movie_id": 1303394,
+        "genre_id": 27
+    },
+    {
+        "id": 1126,
+        "alt": "https://movie.douban.com/subject/1303394/",
+        "year": 1993,
+        "title": "青蛇",
+        "rating": "8.4",
+        "original_title": "青蛇",
+        "directors": "徐克",
+        "casts": "张曼玉,王祖贤,赵文卓",
+        "image": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p584021784.jpg",
+        "name": "运动",
+        "movie_id": 1303394,
+        "genre_id": 27
+    },
+    {
+        "id": 1126,
+        "alt": "https://movie.douban.com/subject/1303394/",
+        "year": 1993,
+        "title": "青蛇",
+        "rating": "8.4",
+        "original_title": "青蛇",
+        "directors": "徐克",
+        "casts": "张曼玉,王祖贤,赵文卓",
+        "image": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p584021784.jpg",
+        "name": "运动",
+        "movie_id": 1303394,
+        "genre_id": 27
+    },
+    {
+        "id": 1126,
+        "alt": "https://movie.douban.com/subject/1303394/",
+        "year": 1993,
+        "title": "青蛇",
+        "rating": "8.4",
+        "original_title": "青蛇",
+        "directors": "徐克",
+        "casts": "张曼玉,王祖贤,赵文卓",
+        "image": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p584021784.jpg",
+        "name": "运动",
+        "movie_id": 1303394,
+        "genre_id": 27
+    },
+    {
+        "id": 1126,
+        "alt": "https://movie.douban.com/subject/1303394/",
+        "year": 1993,
+        "title": "青蛇",
+        "rating": "8.4",
+        "original_title": "青蛇",
+        "directors": "徐克",
+        "casts": "张曼玉,王祖贤,赵文卓",
+        "image": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p584021784.jpg",
+        "name": "运动",
+        "movie_id": 1303394,
+        "genre_id": 27
+    },
+    {
+        "id": 1126,
+        "alt": "https://movie.douban.com/subject/1303394/",
+        "year": 1993,
+        "title": "青蛇",
+        "rating": "8.4",
+        "original_title": "青蛇",
+        "directors": "徐克",
+        "casts": "张曼玉,王祖贤,赵文卓",
+        "image": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p584021784.jpg",
+        "name": "运动",
+        "movie_id": 1303394,
+        "genre_id": 27
+    },
+    {
+        "id": 1126,
+        "alt": "https://movie.douban.com/subject/1303394/",
+        "year": 1993,
+        "title": "青蛇",
+        "rating": "8.4",
+        "original_title": "青蛇",
+        "directors": "徐克",
+        "casts": "张曼玉,王祖贤,赵文卓",
+        "image": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p584021784.jpg",
+        "name": "运动",
+        "movie_id": 1303394,
+        "genre_id": 27
+    },
+    {
+        "id": 1126,
+        "alt": "https://movie.douban.com/subject/1303394/",
+        "year": 1993,
+        "title": "青蛇",
+        "rating": "8.4",
+        "original_title": "青蛇",
+        "directors": "徐克",
+        "casts": "张曼玉,王祖贤,赵文卓",
+        "image": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p584021784.jpg",
+        "name": "运动",
+        "movie_id": 1303394,
+        "genre_id": 27
+    },
+    {
+        "id": 1126,
+        "alt": "https://movie.douban.com/subject/1303394/",
+        "year": 1993,
+        "title": "青蛇",
+        "rating": "8.4",
+        "original_title": "青蛇",
+        "directors": "徐克",
+        "casts": "张曼玉,王祖贤,赵文卓",
+        "image": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p584021784.jpg",
+        "name": "运动",
+        "movie_id": 1303394,
+        "genre_id": 27
+    },
+    {
+        "id": 1126,
+        "alt": "https://movie.douban.com/subject/1303394/",
+        "year": 1993,
+        "title": "青蛇",
+        "rating": "8.4",
+        "original_title": "青蛇",
+        "directors": "徐克",
+        "casts": "张曼玉,王祖贤,赵文卓",
+        "image": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p584021784.jpg",
+        "name": "运动",
+        "movie_id": 1303394,
+        "genre_id": 27
+    }
+]
+addRecommends(movies);
--- detail/test.js
+++ detail/test.js
@@ -2,8 +2,50 @@
  * Created by ashui on 17-8-2.
  */
 'use strict';
+const movies=[{
+    "id": 837,
+    "alt": "https://movie.douban.com/subject/1316510/",
+    "year": 1993,
+    "title": "射雕英雄传之东成西就",
+    "rating": "8.7",
+    "original_title": "射鵰英雄傳之東成西就",
+    "directors": "刘镇伟",
+    "casts": "梁朝伟,林青霞,张国荣",
+    "image": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p1993903133.jpg",
+    "name": "运动",
+    "movie_id": 1316510,
+    "genre_id": 27
+},
+    {
+        "id": 1044,
+        "alt": "https://movie.douban.com/subject/1306249/",
+        "year": 1993,
+        "title": "唐伯虎点秋香",
+        "rating": "8.4",
+        "original_title": "唐伯虎點秋香",
+        "directors": "李力持",
+        "casts": "周星驰,巩俐,陈百祥",
+        "image": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p1946455272.jpg",
+        "name": "运动",
+        "movie_id": 1306249,
+        "genre_id": 27
+    },
+    {
+        "id": 1126,
+        "alt": "https://movie.douban.com/subject/1303394/",
+        "year": 1993,
+        "title": "青蛇",
+        "rating": "8.4",
+        "original_title": "青蛇",
+        "directors": "徐克",
+        "casts": "张曼玉,王祖贤,赵文卓",
+        "image": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p584021784.jpg",
+        "name": "运动",
+        "movie_id": 1303394,
+        "genre_id": 27
+    }]
 describe('recommentd test',()=>{
     it("test environment should ok",()=>{
-        expect('abc').toEqual("bca");
+        expect().toEqual("bca");
     });
 });
