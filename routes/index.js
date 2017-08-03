c3735232e5c8172f62a51df85972905df2ec7ee6
梁凌锐 <392425595@qq.com>
2017-08-03 11:21:25 +0800

    按id查询类别信息

--- routes/index.js
+++ routes/index.js
@@ -18,51 +18,20 @@ router.get('/movies/genre/:genreId', function(req, res) {
 router.get('/genres', function(req, res) {
 
 });
-//根据id查类别----"genres?genreId=" + genreId
+//根据id查类别----"genres?genreId=" + genreId    //返回的数据:{"data":{"id":1,"name":"剧情"}}
 router.get('/genres/:genreId', function(req, res) {
-
+    let genreId = req.params.genreId;
+    req.models.Genre.find({id:genreId}, function(error, genres) {
+        if(genres.length===1){
+            res.status(200).json({data:genres[0]})
+        }else {
+            res.status(200).json({data:genres[0]})
+        }
+    });
 });
 //查询某电影的类别----"genres/movie/?movieId=" + movieId
 router.get('/genres/movie/:movieId', function(req, res) {
 
 });
-
-// router.get('/movies', function (req, res) {
-//     req.models.movie.find({id:1291560}, function(err, movie) {
-//         if (err) {
-//             res.send(err)
-//         }
-//         console.log(movie);
-//         res.json(movie);
-//     });
-// });
-//
-// router.get('/genre', function (req, res) {
-//     // req.models is a reference to models used above in define()
-//     req.models.genre.find({}, function(error, genre) {
-//         console.log(genre);
-//         res.json(genre);
-//     });
-// });
-//
-// router.get('/movie/horror', function (req, res) {
-//     // req.models is a reference to models used above in define()
-//     req.models.movie_genre.find({genre_id:9},function(err, movie_genreIds){
-//         if (err) {
-//             res.send(err)
-//         }
-//         let movieIds = [];
-//         for (let i = 0; i<movie_genreIds.length; i++) {
-//             movieIds.push(movie_genreIds[i].movie_id);
-//         }
-//         console.log(movie_genreIds)
-//         console.log(movieIds);
-//         req.models.movie.find({id:movieIds},function(err,movies){
-//             console.log(movies);
-//             res.json(movies);
-//         })
-//     })
-// });
-
 
 module.exports = router;
